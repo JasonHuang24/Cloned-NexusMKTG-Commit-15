0a62833ad8dadc00bcebcf2c2b74bf6087c597fb
Jason <33882681+JasonHuang24@users.noreply.github.com>
2022-10-23 22:36:43 -0600

    Commit 15.3 -It works!

--- controllers/editAdminController.js
+++ controllers/editAdminController.js
@@ -1,13 +1,18 @@
 const EditAdmin = require('../models/editAdmin'); //EditAdmin Section
 
-
-
 //order matters must be above :id
 const admin_edit_webpage_get = (req, res) => {
   res.render('blogs/edit-webpage', {title: 'Edit a web page'});
 }
 const admin_edit_webpage_get2 = (req, res) => {
-  res.render('website portfolio/wholesome-food-services', {title: 'Wholesome Food Services'});
+  EditAdmin.find()
+  .then((result) => {
+    res.render('website portfolio/wholesome-food-services', {title: 'Wholesome Food Services', editAdmins: result});
+  })
+  .catch(err => {
+    res.status(404).render('404', {title: 'Blog not found'});
+    //console.log(err);
+  })
 }
 
 const editAdmin_create_post = (req, res) => {
